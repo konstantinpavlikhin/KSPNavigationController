523db2a2ebea4e808b5de983447479049501dbda
Konstantin Pavlikhin <k.pavlikhin@gmail.com>
2015-12-04 22:10:00 +0300

    Add a KSPNavigationController framework target.

---
+++ KSPNavigationControllerFramework.h
@@ -0,0 +1,28 @@
+//
+//  KSPNavigationControllerFramework.h
+//  KSPNavigationController
+//
+//  Created by Konstantin Pavlikhin on 04/12/15.
+//  Copyright © 2015 Konstantin Pavlikhin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KSPNavigationController.
+FOUNDATION_EXPORT double KSPNavigationControllerVersionNumber;
+
+//! Project version string for KSPNavigationController.
+FOUNDATION_EXPORT const unsigned char KSPNavigationControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework.
+#import <KSPViewController.h>
+
+#import <KSPViewController+Private.h>
+
+#import <KSPNavViewController.h>
+
+#import <KSPNavViewController+Private.h>
+
+#import <KSPNavigationControllerDelegate.h>
+
+#import <KSPNavigationController.h>
